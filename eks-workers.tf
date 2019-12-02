71e841714d3eb6b28e8d20030a8a4317bd66be23
kathigit2 <57824170+kathigit2@users.noreply.github.com>
2019-12-02 12:56:31 +0530

    Update eks-workers.tf

--- eks-workers.tf
+++ eks-workers.tf
@@ -26,7 +26,7 @@ resource "aws_launch_configuration" "demo" {
   associate_public_ip_address = true
   iam_instance_profile = aws_iam_instance_profile.demo-node.name
   image_id = data.aws_ami.eks-worker.id
-  instance_type = "t2.large"
+  instance_type = "t2.medium"
   name_prefix = "terraform-eks-demo"
   security_groups = [aws_security_group.demo-node.id]
   user_data_base64 = base64encode(local.demo-node-userdata)
@@ -39,8 +39,8 @@ resource "aws_launch_configuration" "demo" {
 resource "aws_autoscaling_group" "demo" {
   desired_capacity = 2
   launch_configuration = aws_launch_configuration.demo.id
-  max_size = 2
-  min_size = 1
+  max_size = 4
+  min_size = 2
   name = "terraform-eks-demo"
   # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
   # force an interpolation expression to be interpreted as a list by wrapping it
